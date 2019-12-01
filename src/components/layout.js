c5526d9894a7ae41bc08f1bdda2db00288c99d63
tappyy <apottage@gmail.com>
2019-12-01 21:28:30 +0000

    more mobile layout, added footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,9 +3,15 @@ import '../styles/layout.scss'
 
 const Layout = ({ children }) => {
   return (
-    <main className='site-content'>
-      {children}
-    </main>
+    <>
+      <main className='site-content'>
+        {children}
+      </main>
+      <footer className='footer'>
+        <p>Built for fun by <a target='_blank' rel="noopener noreferrer" href='https://www.onewiththebeard.dev/'>Andy</a> 🤓</p>
+        <p>Source available on <a target='_blank' rel="noopener noreferrer" href='https://github.com/tappyy/what-is-anne-up-to'>Github</a> 💜</p>
+      </footer>
+    </>
   )
 }
 
